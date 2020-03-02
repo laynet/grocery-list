458dab3b43724ca04f62a3361a5bee188145c7ae
Layne <layneingramtaylor@gmail.com>
2020-03-02 13:30:20 -0600

    index todo views ready

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,9 +4,10 @@ const base = "http://localhost:3000";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("shoudl return status code 200", done => {
+    it("shoudl return status code 200 and have 'INDEX TODO' in the body of the response ", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("INDEX TODO");
         done();
       });
     });
